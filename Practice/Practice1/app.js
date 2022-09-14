958c0fcca6bc868cea156763a01a1ed4e6ba6a55
BeautyHanna <65430214+BeautyHanna@users.noreply.github.com>
2022-09-14 12:28:40 -0400

    Edited Comments and codes

--- Practice/Practice1/app.js
+++ Practice/Practice1/app.js
@@ -1,4 +1,3 @@
-
 // Object Version
 // var myDog1 = {
 //     name: "Vido",
@@ -17,24 +16,49 @@
 
 
 // Class Version
-class Dog {
-    constructor(name,height,color){
+// class Dog {
+//     constructor(name,height,color){
+//         this.name = name;
+//         this.height = height; 
+//         this.color = color;
+//     }
+
+//     bark(){
+//         console.log(this.name + " woofs!");
+//     }
+// }
+// var myDog2 = new Dog("Giselle",10,"yellow");
+
+// for (var i = 0; i < 400; i++){
+//     var myDog3 = new Dog("Clifford #" + i, 400, "red");
+//     myDog3.bark();
+// }
+
+
+// console.log(myDog2);
+// myDog2.bark();
+
+
+
+class Animal{
+    constructor(name,weight,color){
         this.name = name;
-        this.height = height; 
+        this.weight = weight;
         this.color = color;
     }
+}
 
+class Dog extends Animal {
+    constructor(name,weight,color){
+        super(name,weight,color);
+        this.kind = "dog";
+    }
     bark(){
-        console.log(this.name + " woofs!");
+        console.log(this.name + " barks!");
     }
 }
-var myDog2 = new Dog("Giselle",10,"yellow");
 
-for (var i = 0; i < 400; i++){
-    var myDog3 = new Dog("Clifford #" + i, 400, "red");
-    myDog3.bark();
-}
+let aDog = new Dog("Rex", 45, "black");
 
-
-console.log(myDog2);
-myDog2.bark();
+console.log(aDog);
+aDog.bark();
