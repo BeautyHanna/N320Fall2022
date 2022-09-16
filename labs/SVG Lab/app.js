8b311c3beb43341e64b69ed864bc69edc6a6269e
BeautyHanna <65430214+BeautyHanna@users.noreply.github.com>
2022-09-15 22:50:34 -0400

    Finished SVG Lab

--- labs/SVG Lab/app.js
+++ labs/SVG Lab/app.js
@@ -6,10 +6,11 @@ class Game {
     searchColor ="#7603F1";
     normalColor ="#41C8F0";
     gameZone = document.getElementById("gameZone");
+    foundBar = new FoundBar();
 
     constructor(){
         //draw the circles
-        for(var i = 0; i <40; i++){
+        for(var i = 0; i <35; i++){
 
             //create circle
             let newCirc = document.createElementNS("http://www.w3.org/2000/svg", "circle");
@@ -42,13 +43,16 @@ class Game {
             })
 
             newCirc.addEventListener("click", (event) => {
-                
+
                 //if the user clicked on the circle with the "looking for" color (the purple color), it'll remove the purple circle
                 if(event.target.dataset.hiddenColor == this.searchColor){
                     event.target.remove();
 
                     //store how many have been clicked on 
                     this.foundCircles++;
+
+                    //update the found UI (found bar)
+                    this.foundBar.setPercent(this.foundCircles / this.totalCircles);
                     
                 }
             })
@@ -60,4 +64,17 @@ class Game {
     }
 }
 
+class FoundBar {
+    element = document.getElementById("foundBar")
+    maxSize = 130;
+    percent = 0;
+
+    setPercent(percent) {
+        this.percent = percent;
+        this.element.setAttribute("width", this.percent * this.maxSize); 
+    }
+}
+
+// let f = new FoundBar();
+// f.setPercent(0)
 let g = new Game();
